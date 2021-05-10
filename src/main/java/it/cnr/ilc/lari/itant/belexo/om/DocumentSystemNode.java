5c4fd7cedea4ede617424e79840d665430d0c534
Cesare <cesare@ctrl-z-bg.org>
2021-05-10 17:41:37 +0200

    first mock api getDocSystem

---
+++ src/main/java/it/cnr/ilc/lari/itant/belexo/om/DocumentSystemNode.java
@@ -0,0 +1,20 @@
+package it.cnr.ilc.lari.itant.belexo.om;
+
+public class DocumentSystemNode {
+    private String name;
+    private String path;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+}
