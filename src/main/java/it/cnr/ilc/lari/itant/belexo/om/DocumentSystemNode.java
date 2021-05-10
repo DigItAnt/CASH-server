063a3b4c63e9f31010b317db5e55751b832aaabc
Cesare <cesare@ctrl-z-bg.org>
2021-05-10 18:24:31 +0200

    more type, children and element_id in Node

--- src/main/java/it/cnr/ilc/lari/itant/belexo/om/DocumentSystemNode.java
+++ src/main/java/it/cnr/ilc/lari/itant/belexo/om/DocumentSystemNode.java
@@ -1,8 +1,20 @@
 package it.cnr.ilc.lari.itant.belexo.om;
 
+import java.util.List;
+
+import javax.validation.constraints.Pattern;
+
 public class DocumentSystemNode {
     private String name;
     private String path;
+    private List<DocumentSystemNode> children;
+    private int element_id;
+
+    static public enum FileFolder {
+        file, folder
+    }
+
+    private FileFolder type;
     
     public String getName() {
         return name;
@@ -17,4 +29,25 @@ public class DocumentSystemNode {
         this.path = path;
     }
 
+    public FileFolder getType() {
+        return type;
+    }
+    public void setType(FileFolder type) {
+        this.type = type;
+    }
+    public List<DocumentSystemNode> getChildren() {
+        return children;
+    }
+    public void setChildren(List<DocumentSystemNode> children) {
+        this.children = children;
+    }
+    
+    public int getElement_id() {
+        return element_id;
+    }
+    public void setElement_id(int element_id) {
+        this.element_id = element_id;
+    }
+
+    
 }
