c64b77f776526680f391684795667bb2f8d43f40
a.tommasi <ale@ctrl-z-bg.org>
2021-06-30 12:59:19 +0200

    Download file now uses elementId.

--- src/main/java/it/cnr/ilc/lari/itant/belexo/om/DownloadFileRequest.java
+++ src/main/java/it/cnr/ilc/lari/itant/belexo/om/DownloadFileRequest.java
@@ -1,18 +1,3 @@
 ﻿package it.cnr.ilc.lari.itant.belexo.om;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class DownloadFileRequest extends UserReqUUID {
-    
-    @JsonProperty("full-path")
-    String fullPath;
-
-    public String getFullPath() {
-        return fullPath;
-    }
-
-    public void setFullPath(String fullPath) {
-        this.fullPath = fullPath;
-    }
-
-}
+public class DownloadFileRequest extends AddFolderRequest {}
