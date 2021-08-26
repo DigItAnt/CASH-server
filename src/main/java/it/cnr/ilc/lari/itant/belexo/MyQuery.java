290da8b3161306e73aa94df4b9af363eb40ac7cd
Cesare <cesare@ctrl-z-bg.org>
2021-08-26 16:15:27 +0200

    query optimization

--- src/main/java/it/cnr/ilc/lari/itant/belexo/MyQuery.java
+++ src/main/java/it/cnr/ilc/lari/itant/belexo/MyQuery.java
@@ -26,8 +26,8 @@ public class MyQuery {
             RowIterator riter = result.getRows();
             while (riter.hasNext()) {
                 Row row = riter.nextRow();
-                Node node = row.getNode("s");
-                System.out.println("NODEEEEEE " + node.getPath());
+                Node node = row.getNode("p");
+                System.out.println("\n\n\nNODEEEEEE \n\n\n" + node.getPath());
                 JcrManager.logProperties(node);
             }
        } catch (Exception e) {
@@ -38,8 +38,9 @@ public class MyQuery {
     }
 
     public static void main(String[] args) {
-        String statement = "SELECT * FROM [nt:base] as s WHERE s.[type]='traditionalID'";
-        //statement = "select * from [nt:unstructured] as p INNER JOIN [nt:unstructured] as c on ISCHILDNODE(c,p) WHERE c.[meta_docid]='123'";
+        String statement = "SELECT * FROM [nt:base] as p WHERE CONTAINS (p.[jcr:xmlcharacters], 'Oscan 7')";
+        statement = "select * from [nt:base] as c INNER JOIN [nt:unstructured] as p on ISDESCENDANTNODE(c,p) WHERE CONTAINS (c.[jcr:xmlcharacters], 'Inscription ItAnt Oscan 7') and p.[mytype] = 'file'";
+        //statement = "select * from [nt:base] as c INNER JOIN [nt:base] as p on ISCHILDNODE(c,p) WHERE CONTAINS (c.[jcr:xmlcharacters], 'Inscription ItAnt Oscan 7')";
         doQuery(statement);
     }
 
