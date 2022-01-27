1711bbd56ad55e1daec880b54a17b61cbd5d3dc8
Cesare <cesare@ctrl-z-bg.org>
2022-01-27 13:19:27 +0100

    put/post/get annotations

--- src/main/java/it/cnr/ilc/lari/itant/belexo/controller/AnnotationController.java
+++ src/main/java/it/cnr/ilc/lari/itant/belexo/controller/AnnotationController.java
@@ -6,7 +6,11 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +27,26 @@ public class AnnotationController {
         return "Lorem ipsum dolor sit amet consectetur adipiscing elit";
     }
 
-    @GetMapping(value="/api/v1/getannotations")
+    @GetMapping(value="/api/v1/annotation")
     public List<Annotation> getAnnotations(@RequestParam String nodeid) {
 
         return new ArrayList<Annotation>();
     }
+
+    @PostMapping(value="/api/v1/annotation")
+    public Annotation createAnnotation(@RequestParam String nodeid, @RequestBody Annotation annotation) {
+        return annotation;
+    }
+
+    @DeleteMapping(value="/api/v1/annotate")
+    public void deleteAnnotation(@RequestParam int annotationID) {
+        log.info("delete annotation " + annotationID);
+    }
+
+    @PutMapping(value="/api/v1/annotation")
+    public Annotation modifyAnnotation(@RequestBody Annotation annotation) {
+        return annotation;
+    }
+
 
 }
