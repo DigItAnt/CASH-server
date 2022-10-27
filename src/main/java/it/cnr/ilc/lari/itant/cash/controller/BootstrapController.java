bd58c1fc69ac4deaf143fddd6ffae6968e2fc51c
Cesare <cesare@ctrl-z-bg.org>
2022-10-27 15:40:36 +0200

    log message template

--- src/main/java/it/cnr/ilc/lari/itant/cash/controller/BootstrapController.java
+++ src/main/java/it/cnr/ilc/lari/itant/cash/controller/BootstrapController.java
@@ -1,7 +1,10 @@
 package it.cnr.ilc.lari.itant.cash.controller;
 
 import java.security.Principal;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +14,15 @@ import it.cnr.ilc.lari.itant.cash.DBManager;
 import it.cnr.ilc.lari.itant.cash.om.DocumentSystemNode;
 import it.cnr.ilc.lari.itant.cash.om.GetDocumentSystemResponse;
 import it.cnr.ilc.lari.itant.cash.om.GetUsersResponse;
+import it.cnr.ilc.lari.itant.cash.utils.LogUtils;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 @CrossOrigin
 @RestController
 public class BootstrapController {
-    
+    private static final Logger log = LoggerFactory.getLogger(BootstrapController.class);
+
     @GetMapping("/rest/ping")
 	String ping() {
 		return "pong";
@@ -25,7 +30,8 @@ public class BootstrapController {
 
 	@GetMapping("/api/getDocumentSystem")
 	public GetDocumentSystemResponse getDocumentSystem(@RequestParam String requestUUID, Principal principal) {
-		//System.out.println("............................................................ User: " + principal.getName());
+		log.info(LogUtils.CASH_INVOCATION_LOG_MSG, requestUUID, principal.getName());
+
 		PodamFactory factory = new PodamFactoryImpl();
 		GetDocumentSystemResponse toret = factory.manufacturePojo(GetDocumentSystemResponse.class);
 		try {
@@ -39,7 +45,9 @@ public class BootstrapController {
 	}
 
 	@GetMapping("/api/getUsers")
-	public GetUsersResponse getUsers(@RequestParam String requestUUID) {
+	public GetUsersResponse getUsers(@RequestParam String requestUUID, Principal principal) {
+		log.info(LogUtils.CASH_INVOCATION_LOG_MSG, requestUUID, principal.getName());
+
 		PodamFactory factory = new PodamFactoryImpl();
 		GetUsersResponse toret = factory.manufacturePojo(GetUsersResponse.class);
 		toret.setRequestUUID(requestUUID);
---
+++ src/main/java/it/cnr/ilc/lari/itant/cash/utils/LogUtils.java
@@ -0,0 +1,6 @@
+package it.cnr.ilc.lari.itant.cash.utils;
+
+public class LogUtils {
+    public static String CASH_INVOCATION_LOG_MSG = "CASH INVOCATION, REQ_ID: {}, USER: {}, CUSTOM MSG: {}";
+    
+}
