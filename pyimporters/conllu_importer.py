064d0733cf89e1ecb948e11b92e572e2fd86f18f
a.tommasi <ale@ctrl-z-bg.org>
2022-11-24 12:43:02 +0100

    Improvements to the importer.

--- pyimporters/conllu_importer.py
+++ pyimporters/conllu_importer.py
@@ -7,6 +7,9 @@ from colorama import Fore, Back, Style
 
 KEY_PFIX = "conllu_"
 
+def print_log(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def get_token(kc, username, password):
     data = {
         'client_id': 'princlient',
@@ -17,9 +20,9 @@ def get_token(kc, username, password):
 
     response = requests.post(kc, data=data)
     if response.status_code != 200:
-        print(Fore.RED + "Error while getting token:" + Style.RESET_ALL,
-              response.text)
-        sys.exit(1)
+        print_log(Fore.RED + "Error while getting token:" + Style.RESET_ALL,
+                  response.text)
+        sys.exit(response.status_code)
     return response.json()['access_token']
 
 def create_file(filename, content, text, destination=0, token="", url=""):
@@ -44,13 +47,14 @@ def create_file(filename, content, text, destination=0, token="", url=""):
     try:   
         response = requests.post(f'{url}api/crud/uploadFile', params=params, headers=headers, files=files, verify=False)
     except Exception as e:
-        print(Fore.RED + "Error while uploading file:", str(e) + Style.RESET_ALL)
+        print_log(Fore.RED + "Error while uploading file:",
+                  str(e) + Style.RESET_ALL)
         sys.exit(1)
     
     if response.status_code != 200:
-        print(Fore.RED + "Error creating file:" + Style.RESET_ALL,
-              response.text)
-        sys.exit(1)
+        print_log(Fore.RED + "Error creating file:" + Style.RESET_ALL,
+                  response.text)
+        sys.exit(response.status_code)
 
     elid = response.json()['node']['element-id']
 
@@ -65,11 +69,11 @@ def create_file(filename, content, text, destination=0, token="", url=""):
     }
     response = requests.post(f'{url}api/v1/unstructured', params=params, headers=headers, json=data, verify=False)
     if response.status_code != 200:
-        print(Style.RED  + "Error uploading text:" + Style.RESET_ALL,
-              response.text, Style.RED + "remove node" + Style.RESET_ALL, elid)
-        sys.exit(1)
-    print(Fore.GREEN + "File created,", len(content),
-          "bytes, id:" + Style.RESET_ALL, elid)
+        print_log(Style.RED  + "Error uploading text:" + Style.RESET_ALL,
+                  response.text, Style.RED + "remove node" + Style.RESET_ALL, elid)
+        sys.exit(response.status_code)
+    print_log(Fore.GREEN + "File created,", len(content),
+              "bytes, id:" + Style.RESET_ALL, elid)
     return (elid, response.json()['unstructuredids']['conllu'])
 
 def create_token(docid, token_text, position, span_from, span_to, token="", url=""):
@@ -98,10 +102,10 @@ def create_token(docid, token_text, position, span_from, span_to, token="", url=
     
     response = requests.post(f'{url}api/v1/token', params=params, headers=headers, json=data, verify=False)
     if response.status_code != 200:
-        print(Fore.RED + "Error creating token:" + Style.RESET_ALL,
-              response.text)
-        sys.exit(1)
-    #print(response.json())
+        print_log(Fore.RED + "Error creating token:" + Style.RESET_ALL,
+                  response.text)
+        sys.exit(response.status_code)
+    #print_log(response.json())
     return response.json()['token']['id']
 
 def create_annotation(docid, span_from, span_to, layer, value, attributes={}, token="", url=""):
@@ -126,15 +130,15 @@ def create_annotation(docid, span_from, span_to, layer, value, attributes={}, to
     }
     
     response = requests.post(f'{url}api/v1/annotation', params=params, headers=headers, json=data, verify=False)
-    #print(data)
-    #print(response.json())
+    #print_log(data)
+    #print_log(response.json())
     if response.status_code != 200:
-        print(Fore.RED + "Error creating annotation:" + Style.RESET_ALL,
-              data,
-              response.text)
-        print(response)
-        print(response.status_code)
-        sys.exit(1)
+        print_log(Fore.RED + "Error creating annotation:" + Style.RESET_ALL,
+                  data,
+                  response.text)
+        print_log(response)
+        print_log(response.status_code)
+        sys.exit(response.status_code)
     return response.json()['annotation']['id']   
 
 #find the first occurrence of target in sentence starting from offset
@@ -171,7 +175,7 @@ def reconstruct_sentence(tokens, offset=0):
             token['__span__to'] = multiword_heads[tid]['__span__to']
             
     ret = text or sentence.strip()
-    print(Fore.GREEN + "Sentence:" + Style.RESET_ALL, ret)
+    print_log(Fore.GREEN + "Sentence:" + Style.RESET_ALL, ret)
     return ret
 
 def parse_conllu_file(filename):
@@ -183,10 +187,10 @@ def parse_conllu_file(filename):
             try:
                 data = parse(text)
             except ParseException as e:
-                print(Fore.RED + "Error while parsing:", str(e) + Style.RESET_ALL)
+                print_log(Fore.RED + "Error while parsing:", str(e) + Style.RESET_ALL)
                 sys.exit(1)
     except IOError as e:
-                print(Fore.RED + "I/O error while reading file:", str(e) + Style.RESET_ALL)
+                print_log(Fore.RED + "I/O error while reading file:", str(e) + Style.RESET_ALL)
                 sys.exit(1)
     return data# reconstruct sentences from tokens
 
@@ -196,8 +200,8 @@ def feat2str(feat):
     return "|".join([k + "=" + v for k, v in feat.items()])
 
 def insert(filename, destination=0, kc_token="", url="", fields=None, sentence_layer="sentences",
-           verbose=False):
-    print(Fore.GREEN + "Parsing file:" + Style.RESET_ALL, filename)
+           verbose=False, test=False):
+    print_log(Fore.GREEN + "Parsing file:" + Style.RESET_ALL, filename)
     data = parse_conllu_file(filename)
     raw = ""
     with open(filename, "r", encoding="utf-8") as f:
@@ -208,9 +212,9 @@ def insert(filename, destination=0, kc_token="", url="", fields=None, sentence_l
         text += reconstruct_sentence(sentence, offset=len(text)) + "\n"
     
     # insert the text into CASH
-    print(Fore.GREEN + "Inserting doc into " + url + ":" + Style.RESET_ALL +
-          "\n", text[:50] + "...")
-    docid, srcid = create_file(filename, raw, text, destination, kc_token, url)
+    print_log(Fore.GREEN + "Inserting doc into " + url + ":" + Style.RESET_ALL +
+              "\n", text[:50] + "...")
+    docid, srcid = create_file(filename, raw, text, destination, kc_token, url) if not test else (0, 0)
 
     tokencount = 1
     for sentence in data:
@@ -226,38 +230,44 @@ def insert(filename, destination=0, kc_token="", url="", fields=None, sentence_l
             for coli, col in enumerate(['form', 'lemma', 'upos', 'xpos',
                                         'feats', 'head', 'deprel', 'deps', 'misc']):
                 if col == 'form': # insert the token
-                    print(Fore.GREEN + "Inserting token:" + Style.RESET_ALL,
-                          token[col] + Fore.GREEN + "@" +
-                          str(span_from) + ":" + str(span_to), end = '')
+                    print_log(Fore.GREEN + "Inserting token:" + Style.RESET_ALL,
+                              token[col] + Fore.GREEN + "@" +
+                              str(span_from) + ":" + str(span_to), end = '')
                     if '__skip__' in token: tokencount -= 1
-                    tokenid = create_token(docid, token[col], tokencount, span_from, span_to, kc_token, url)
+                    tokenid = create_token(docid, token[col], tokencount,
+                                           span_from, span_to, kc_token, url) if not test else 0
                     tokencount += 1
                 elif col == 'feats' or col == 'misc': # insert the features
                     layer = fields[coli-1] if fields else col
                     value = feat2str(token[col])
-                    if value != '' and value is not None:
-                        if verbose: print("\nannotation:", layer, value, end='')
-                        create_annotation(docid, span_from, span_to, layer, value, token[col], kc_token, url)
+                    if value != '' and value is not None and (value != '_' or token['form'] == '_'):
+                        if verbose: print_log("\nannotation:", layer, Style.RESET_ALL,
+                                              value, Fore.GREEN, end='')
+                        if not test:
+                            create_annotation(docid, span_from, span_to,
+                                              layer, value, token[col], kc_token, url)
                         ann_cnt += 1
                 else: # insert the annotation
                     layer = fields[coli-1] if fields else col
                     value = token[col]
-                    if value != '' and value is not None:
-                        if verbose: print("\nannotation:", layer, value, end='')
-                        create_annotation(docid, span_from, span_to, layer, value, {}, kc_token, url)
+                    if value != '' and value is not None and (value != '_' or token['form'] == '_'):
+                        if verbose: print_log("\nannotation:", layer, Style.RESET_ALL, value, Fore.GREEN, end='')
+                        if not test:
+                            create_annotation(docid, span_from, span_to,
+                                              layer, value, {}, kc_token, url)
                         ann_cnt += 1
-                    if value != None:
-                        create_annotation(docid, span_from, span_to, layer, value, {}, kc_token, url)
-                        ann_cnt += 1
-            if verbose: print(Style.RESET_ALL)
-            else: print(" with", ann_cnt, "annotations" + Style.RESET_ALL)
+            if verbose: print_log(Style.RESET_ALL)
+            else: print_log(" with", ann_cnt, "annotations" + Style.RESET_ALL)
 
         # insert the sentence annotation
         layer = sentence_layer
         value = "sentence"
-        create_annotation(docid, sentence_start, sentence_end, layer, value, sentence.metadata, kc_token, url)
-        print(Fore.BLUE + Style.BRIGHT + "Inserting sentence annotation:" + Style.RESET_ALL,
-              sentence_start, sentence_end)
+        if not test:
+            create_annotation(docid, sentence_start, sentence_end,
+                              layer, value, sentence.metadata, kc_token, url)
+        print_log(Fore.BLUE + Style.BRIGHT + "Inserting sentence annotation:" + Style.RESET_ALL,
+                  sentence_start, sentence_end)
+    return docid
                     
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -268,7 +278,7 @@ if __name__ == "__main__":
                            default=None)
     argparser.add_argument("-s", "--sentence-layer", help="name of the sentence layer (default: sentences)",
                            default="sentences")
-    argparser.add_argument("-d", "--destination", default=0, help="folder destination ID")
+    argparser.add_argument("-d", "--destination", default=0, help="destination folder ID")
     argparser.add_argument("-u", "--username", help="username")
     argparser.add_argument("-p", "--password", help="password")    
     argparser.add_argument("-c", "--cash-url", default="https://lari2.ilc.cnr.it/cash/",
@@ -277,22 +287,25 @@ if __name__ == "__main__":
                            default="https://lari2.ilc.cnr.it/auth/realms/princnr/protocol/openid-connect/token",
                            help="CASH URL")
     argparser.add_argument("-v", "--verbose", help="Verbose mode (log more)", action="store_true", default=False)
+    argparser.add_argument("--dry", help="Dry run, do not insert", action="store_true", default=False)
     args = argparser.parse_args()
 
     fields = None
     if ( args.fields ):
         fields = [f.strip() for f in args.fields.split(",")]
         if len(fields) != 8:
-            print(Fore.RED + "Fields must be 8 comma-separated values for " +
-                  "lemma,upos,xpos,feats,head,deprel,deps,misc" + Style.RESET_ALL)
+            print_log(Fore.RED + "Fields must be 8 comma-separated values for " +
+                      "lemma,upos,xpos,feats,head,deprel,deps,misc" + Style.RESET_ALL)
             sys.exit(1)
 
     try:
         token = get_token(args.keycloak_url, args.username, args.password)
     except Exception as e:
-        print(Fore.RED + "Error while getting OAuth token:", str(e) + Style.RESET_ALL)
+        print_log(Fore.RED + "Error while getting OAuth token:", str(e) + Style.RESET_ALL)
         sys.exit(1)
             
-    insert(args.filename, destination=args.destination, kc_token=token,
-           url=args.cash_url, fields=args.fields, sentence_layer=args.sentence_layer,
-           verbose=args.verbose)
+    did = insert(args.filename, destination=args.destination, kc_token=token,
+                 url=args.cash_url, fields=args.fields,
+                 sentence_layer=args.sentence_layer,
+                 verbose=args.verbose, test=args.dry)
+    print(did)
