333372c2cd38099af5a0a9f28583a5f6c8bb883a
Cesare <cesare@ctrl-z-bg.org>
2023-09-04 16:36:00 +0200

    custom message for invalid param exception

--- src/main/java/it/cnr/ilc/lari/itant/cash/exc/InvalidParamException.java
+++ src/main/java/it/cnr/ilc/lari/itant/cash/exc/InvalidParamException.java
@@ -3,7 +3,14 @@ package it.cnr.ilc.lari.itant.cash.exc;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason="One of the parameters is invalid")
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
 public class InvalidParamException extends RuntimeException {
+    public InvalidParamException() {
+        super("One of the parameters is invalid");
+    }
     
+    public InvalidParamException(String message) {
+        super(message);
+    }
+
 }
